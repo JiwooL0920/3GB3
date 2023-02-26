69cc56bc901431ac9aa0fd1f0db98730bf4ee013
Yichen Jiang <yichen@YichendeMacBook-Pro.local>
2023-02-26 18:57:19 -0500

    add convo01c&d

--- 3GB3/Assets/DialogueSystem/DialogueLine.cs
+++ 3GB3/Assets/DialogueSystem/DialogueLine.cs
@@ -1,12 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 
 
 [System.Serializable]
 
-public class DialogueLine : MonoBehaviour
+public class DialogueLine
 {
 	public Speaker speaker;
 	[TextArea]
