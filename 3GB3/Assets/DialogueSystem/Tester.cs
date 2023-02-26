f36092bc9db7d6d156ecfed07e543ad648d69c4b
Yichen Jiang <yichen@YichendeMacBook-Pro.local>
2023-02-26 18:35:53 -0500

    Dialogue system update

---
+++ 3GB3/Assets/DialogueSystem/Tester.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester : MonoBehaviour
+{
+
+    public Conversation convo;
+
+    public void StartConvo()
+    {
+        DialogueManager.StartConversation(convo);
+    }
+}
