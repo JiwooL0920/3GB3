1828a952058bb6a43bc6ddbc858791e987424eb7
Yichen Jiang <yichen@YichendeMacBook-Pro.local>
2023-02-27 15:59:48 -0500

    debug

--- 3GB3/Assets/Script/LoadScene.cs
+++ 3GB3/Assets/Script/LoadScene.cs
@@ -11,7 +11,7 @@ public class LoadScene: MonoBehaviour
 
     public void LoadRoom()
     {
-        SceneManager.LoadScene("Room");
+        SceneManager.LoadScene("Room_afterwork");
     }
 
     public void LoadRoad()
