2904ae1644272b193b90d6b2c889896475821db4
Yuhong11111 <nieyuhong2000@gmail.com>
2023-02-27 13:23:33 -0500

    health display

--- 3GB3/Assets/Script/PlayerHealth.cs
+++ 3GB3/Assets/Script/PlayerHealth.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour {
     public int health;
     public int maxHealth;
+    public TMPro.TMP_Text uiPlayerHealth;
 
     public void OnHit(int dmg) {
         health-= dmg;
+        uiPlayerHealth.text = health.ToString();
         if(health <= 0) {
             IsDie();
         }
