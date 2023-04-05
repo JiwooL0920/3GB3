7462f160cefbe2a18bffe40f9e6ed480bc97d06d
Yuhong11111 <nieyuhong2000@gmail.com>
2023-04-04 20:14:41 -0400

    question mark

---
+++ 3GB3/Assets/Script/On_Click.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class On_Click : MonoBehaviour
+{
+    public GameObject questionPop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Click(){
+        questionPop.SetActive(true);
+    }
+    public void UnClick(){
+        questionPop.SetActive(false);
+    }
+}
