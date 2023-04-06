ddebc681c276f7f17e759ae0b662c831a668fedd
Yichen Jiang <yichen@YichendeMacBook-Pro.local>
2023-04-05 21:52:47 -0400

    dialogue box movements

--- 3GB3/Assets/DialogueSystem/DialogueManager.cs
+++ 3GB3/Assets/DialogueSystem/DialogueManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 using TMPro;
+using System.Collections;
 
 
 
@@ -11,18 +12,21 @@ using TMPro;
 public class DialogueManager : MonoBehaviour
 {
 	public TextMeshProUGUI speakerName, dialogue, navButtonText;
-	public Image speakerSprite;
+	public Image speakerSprite; 
+
 
 	private int currentIndex;
 	private Conversation currentConvo;
 	private static DialogueManager instance;
-	
+	private Animator anim;
 	
 	private void Awake()
 	{
 		if(instance == null)
 		{
 			instance = this;
+			anim = GetComponent<Animator>();
+			instance.anim.SetBool("isOpen", false);
 		}
 		else
 		{
@@ -32,6 +36,8 @@ public class DialogueManager : MonoBehaviour
 
 	public static void StartConversation(Conversation convo)
 	{
+		instance.anim.SetBool("isOpen", true);
+		Debug.Log("load");
 		instance.currentIndex = 0;
 		instance.currentConvo = convo;
 		instance.speakerName.text = "";
@@ -45,13 +51,37 @@ public class DialogueManager : MonoBehaviour
     {
 		if(currentIndex > currentConvo.GetLength())
         {
+			instance.anim.SetBool("isOpen",false);
 			return;
         }
 		speakerName.text = currentConvo.GetLineByIndex(currentIndex).speaker.GetName();
 		dialogue.text = currentConvo.GetLineByIndex(currentIndex).dialogue;
 		speakerSprite.sprite = currentConvo.GetLineByIndex(currentIndex).speaker.GetSprite();
 		currentIndex++;
+
+		if(currentIndex >= currentConvo.GetLength())
+		{
+			//navButtonText.text = "X";
+		}
     }
+/*
+	private IEnumerator TypeText(string text)
+	{
+		dialogue.text = "";
+		bool complete = false;
+		int index = 0;
 
+		while(!complete)
+		{
+			dialogue.text += text[index];
+			index++;
 
+			yield return new WaitForSeconds(0.02f);
+			if(index == text.Length -1)
+			complete = true;
+		}
+
+	}
+	
+*/
 }
